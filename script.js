21aebc64b13b58ed6fe3e8d3600c0bab8dc2a986
RadhaShinde-spec <radheyshinde010@gmail.com>
2024-06-26 21:02:33 +0530

    message

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function convertCurrency() {
     const inr = document.getElementById('inr').value;
     const exchangeRate = 0.012; // Example exchange rate (1 INR = 0.012 USD)
     const usd = inr * exchangeRate;
-    document.getElementById('result').innerText = `${inr} INR = ${usd.toFixed(2)} USD`;
+    document.getElementById('result').value = `${usd.toFixed(2)} USD`;
 }
